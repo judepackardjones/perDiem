08e2ded3372f9a2ab736fc320615c531e5ae82d8
Jude Packard-Jones <judepackardjones@gmail.com>
2024-05-27 10:26:23 -0400

    - compare_dyn_any from utils

--- src/utils.rs
+++ src/utils.rs
@@ -4,18 +4,25 @@ use std::any::Any;
 pub fn floor(num: f32) -> i32 {
     (num - (num % 1.0)) as i32
 }
-pub fn compare_dyn_any_values(a: &dyn Any, b: &dyn Any) -> Result<bool, &'static str> {
-    if let Some(a_value) = a.downcast_ref::<i8>() {
-        if let Some(b_value) = b.downcast_ref::<i8>() {
-            return Ok(a_value == b_value);
-        }
-    } else if let Some(a_value) = a.downcast_ref::<i16>() {
-        if let Some(b_value) = b.downcast_ref::<i16>() {
-            return Ok(a_value == b_value);
-        }
-    }
-    Err("Values not of same type") // Values are not of the same type or the downcast failed.
-}
+// not useful right now but I want to keep
+// pub fn compare_dyn_any_values(a: &dyn Any, b: &dyn Any) -> Result<bool, &'static str> {
+//     if let Some(a_value) = a.downcast_ref::<i8>() {
+//         if let Some(b_value) = b.downcast_ref::<i8>() {
+//             return Ok(a_value == b_value);
+//         }
+//     } else if let Some(a_value) = a.downcast_ref::<i16>() {
+//         if let Some(b_value) = b.downcast_ref::<i16>() {
+//             return Ok(a_value == b_value);
+//         }
+//     } 
+//     else if let Some(a_value) = a.downcast_ref::<i32>() {
+//         if let Some(b_value) = b.downcast_ref::<i32>() {
+//             return Ok(a_value == b_value);
+//         }
+//     } 
+//     Err("Values not of same type") // Values are not of the same type or the downcast failed.
+    
+// }
 
 impl dateStr for &str {
     fn with_separators(&self, separator: &char) -> String {
